0a12a7b1cfa1c3e4a4fd7e02762fdb73c60d7e01
John Bolton <jambolo@users.noreply.github.com>
2021-12-02 16:58:57 -0800

    WIP

---
+++ test/TestBeast.cpp
@@ -0,0 +1,22 @@
+#include "targetver.h"
+#include "CppUnitTest.h"
+
+#include "Beast.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace
+{
+}
+
+namespace TestBeast
+{
+    TEST_CLASS(Beast)
+    {
+    public:
+        TEST_METHOD(Beast_simplify)
+        {
+            Assert::Fail(L"Not yet implemented.");
+        }
+    };
+}
